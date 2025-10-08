0c03200b4f95c5cecc46cffc420c98c575b44f61
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-10-08 20:03:15 +0300

    scripts/gender-names.go: updates to send requests in batches

--- scripts/gender-names.go
+++ scripts/gender-names.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -17,15 +18,15 @@ import (
 )
 
 type Args struct {
-	Start, End int
+	Start, End, Batch int
 }
 
 type Question struct {
-	MemberName string `json:"member-name" jsonschema:"description=Claimed to be a human name"`
+	MemberNames []string `json:"member-names" jsonschema:"description=Claimed to be a human name"`
 }
 
 type Answer struct {
-	Gender string `json:"gender" jsonschema:"enum=male,female,unisex,other"`
+	Genders map[string]string `json:"genders" jsonschema:"description=A dictionary from member names to either of male, female, unisex or other"`
 }
 
 type OutputFiles struct {
@@ -40,6 +41,7 @@ func Main() error {
 	args := Args{}
 	flag.IntVar(&args.Start, "start", 0, "start index")
 	flag.IntVar(&args.End, "end", -1, "start index")
+	flag.IntVar(&args.Batch, "batch", 10, "batch")
 	flag.Parse()
 
 	api := &googlegenai.GoogleAI{
@@ -54,7 +56,11 @@ func Main() error {
 
 	flow := genkit.DefineFlow(g, "AnswerGeneratorFlow",
 		func(ctx context.Context, q *Question) (*Answer, error) {
-			p := fmt.Sprintf(`Create an Answer with the following requirements: Member Name: %s`, q.MemberName)
+			j, err := json.Marshal(q.MemberNames)
+			if err != nil {
+				return nil, fmt.Errorf("encoding question into json: %w", err)
+			}
+			p := fmt.Sprintf(`Create an Answer with the following requirements: Member Names: %s`, j)
 			a, _, err := genkit.GenerateData[Answer](ctx, g, ai.WithPrompt(p))
 			if err != nil {
 				return nil, fmt.Errorf("genkit.GenerateData: %w", err)
@@ -86,11 +92,15 @@ func Main() error {
 	defer o.Female.Close()
 
 	included, excluded := 0, 0
+	pct := -1
+	batch := 0
 
 	defer func() {
-		fmt.Println("total    :", included+excluded)
-		fmt.Println("included :", included)
-		fmt.Println("excluded :", excluded)
+		fmt.Println("total :", included+excluded)
+		fmt.Println("incl. :", included)
+		fmt.Println("excl. :", excluded)
+		fmt.Println("pct.  :", pct)
+		fmt.Println("batch :", batch)
 	}()
 
 	memberNames := strings.Split(string(f), "\n")
@@ -98,32 +108,37 @@ func Main() error {
 		args.End = len(memberNames)
 	}
 	memberNames = memberNames[args.Start:args.End]
-	pct := -1
 
-	for i, memberName := range memberNames {
+	for b := 0; b*args.Batch < len(memberNames); b++ {
+		var (
+			from = min(len(memberNames), args.Batch*(b))
+			to   = min(len(memberNames), args.Batch*(b+1))
+		)
 		q := &Question{
-			MemberName: memberName,
+			MemberNames: memberNames[from:to],
 		}
 		a, err := flow.Run(context.Background(), q)
 		if err != nil {
 			log.Fatalf("flow.Run: %v", err)
 		}
 
-		switch a.Gender {
-		case "male":
-			fmt.Fprintln(o.Male, memberName)
-			included += 1
-		case "female":
-			fmt.Fprintln(o.Female, memberName)
-			included += 1
-		case "unisex", "other":
-			excluded += 1
-		default:
-			fmt.Printf("WARNING: unexpected answer from LLM: %q\n", a.Gender)
-			excluded += 1
+		for name, gender := range a.Genders {
+			switch gender {
+			case "male":
+				fmt.Fprintln(o.Male, name)
+				included += 1
+			case "female":
+				fmt.Fprintln(o.Female, name)
+				included += 1
+			case "unisex", "other":
+				excluded += 1
+			default:
+				fmt.Printf("WARNING: unexpected answer from LLM: %q\n", gender)
+				excluded += 1
+			}
 		}
 
-		if pct2 := percentage(i, len(memberNames)); pct2 > pct {
+		if pct2 := percentage(included+excluded, len(memberNames)); pct2 > pct {
 			pct = pct2
 			fmt.Printf("progress: %%%d\n", pct)
 		}
